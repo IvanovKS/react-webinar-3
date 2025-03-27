336143f463b3f9e6cb6beae03571838844572b32
IvanovKS <Skitchi@mail.ru>
2025-03-27 21:22:21 +0300

    Change styles for buttons, show prices, delete evens for buttons

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -4,40 +4,17 @@ import { plural } from '../../utils';
 import './style.css';
 
 function Item(props) {
-  // Счётчик выделений
-  const [count, setCount] = useState(0);
-
-  const callbacks = {
-    onClick: () => {
-      props.onSelect(props.item.code);
-      if (!props.item.selected) {
-        setCount(count + 1);
-      }
-    },
-    onDelete: e => {
-      e.stopPropagation();
-      props.onDelete(props.item.code);
-    },
-  };
 
   return (
     <div
       className={'Item' + (props.item.selected ? ' Item_selected' : '')}
-      onClick={callbacks.onClick}
     >
-      <div className="Item-code">{props.item.code}</div>
       <div className="Item-title">
         <b>{props.item.title}</b>
-        {count
-          ? ` | Выделяли ${count} ${plural(count, {
-              one: 'раз',
-              few: 'раза',
-              many: 'раз',
-            })}`
-          : ''}
       </div>
+      <div className="Item-price">{props.item.price.toLocaleString("ru-RU")} &#8381;</div>
       <div className="Item-actions">
-        <button onClick={callbacks.onDelete}>Удалить</button>
+        <button>Добавить</button>
       </div>
     </div>
   );
